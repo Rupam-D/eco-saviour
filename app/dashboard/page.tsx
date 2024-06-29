8663899c9500479b383d7a5119678b2be47673f2
Rupam <rupamdas.dev@gmail.com>
2024-06-30 03:34:44 +0530

    bug fixed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,8 @@ import LeftSidebar from "@/components/LeftSidebar"
 
 const Dashboard = async () => {
   const userInfo = await trpc_server.syncToDb()
-//   console.log(userInfo, "first")
+  console.log(userInfo.dbUser)
+  
 
   return (
     <main className='bg-gray-100 flex flex-row p-8 gap-x-10'>
@@ -14,7 +15,7 @@ const Dashboard = async () => {
 
       <LeftSidebar />
       <div className='bg-white  rounded-xl shadow-md'>
-        <Dashboardmain name={userInfo.dbUser.name} />
+       {userInfo.dbUser&& <Dashboardmain name={userInfo.dbUser.name} />}
       </div>
       {/* <CarbonCalculator /> */}
     </main>
